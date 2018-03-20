4a6c79950d1f818753671300bd79b1aa14e7e819
Ian <imatthews009@gmail.com>
2018-03-19 19:49:13 -0700

    added toppings, cheese, sauce, and crust to class

--- lib/pizza-generator.rb
+++ lib/pizza-generator.rb
@@ -1,34 +1,58 @@
 class Pizza
-  # veggies: ["Artichoke Hearts", "Avocado", "Broccoli", "Capers", "Capicolla", "Cherry Tomatoes", "Eggplant", "Green Peppers", "Jalapeno Peppers", "Mushrooms", "Olives", "Onions", "Pineapple", "Red Onions", "Red Peppers", "Roasted Garlic", "Spinach", "Zucchini"]
-  # garnishes: ["Basil", "Bay Leaf", "Dried Chili", "Chives", "Cilantro", "Garlic", "Oregano", "Parsley", "Pepper"]
-  # proteins: ["Anchovies", "Bacon", "Beef", "Chorizo", "Ham", "Meatballs", "Pepperoni", "Proscuitto", "Sausage"]
 
   TOPPINGS = ["Anchovies", "Bacon", "Beef", "Chorizo", "Ham", "Meatballs", "Pepperoni", "Proscuitto", "Sausage", "Basil", "Bay Leaf", "Dried Chili", "Chives", "Cilantro", "Garlic", "Oregano", "Parsley", "Pepper", "Artichoke Hearts", "Avocado", "Broccoli", "Capers", "Capicolla", "Cherry Tomatoes", "Eggplant", "Green Peppers", "Jalapeno Peppers", "Mushrooms", "Olives", "Onions", "Pineapple", "Red Onions", "Red Peppers", "Roasted Garlic", "Spinach", "Zucchini"]
   CRUSTS = ["Deep Dish", "Double Decker", "Pan", "Thin Crust", "Hand Tossed", "Ultra Thin Crust"]
   CHEESES = ["Cheddar", "Feta", "Goat Cheese", "Monetery Jack", "Parmesan", "Provolone", "Ricotta"]
   SAUCES = ["Tomato", "Marinera", "White Sauce", "Pesto"]
 
-  # def initialize
+  attr_reader :toppings, :cheese, :sauce, :crust
 
-  # end
-
-  def self.generate
-    Pizza.new
-  end
-
-  def toppings(quantity=2)
-    TOPPINGS.sample(quantity)
-  end
-
-  def cheese 
-    CHEESES.sample
+  def initialize(toppings, cheese, sauce, crust)
+    @toppings = toppings
+    @cheese = cheese 
+    @sauce = sauce
+    @crust = crust
   end 
 
-  def sauce 
-    SAUCES.sample
+  def self.generate(quantity=2)
+    
+    toppings = Pizza.toppings(quantity)
+    cheese = Pizza.cheese
+    sauce = Pizza.sauce
+    crust = Pizza.crust
+
+    Pizza.new(toppings, cheese, sauce, crust)
+
   end
 
-  def crust 
-    CRUSTS.sample
+  def ingredients
+    pizza_hash = {
+      toppings: @toppings,
+      cheese: @cheese,
+      sauce: @sauce,
+      crust: @crust
+    }
+
+    return pizza_hash
+  end
+
+  private 
+  class << self
+
+    def toppings(quantity)
+      TOPPINGS.sample(quantity)
+    end
+
+    def cheese 
+      CHEESES.sample
+    end 
+
+    def sauce 
+      SAUCES.sample
+    end
+
+    def crust 
+      CRUSTS.sample
+    end
   end
 end
